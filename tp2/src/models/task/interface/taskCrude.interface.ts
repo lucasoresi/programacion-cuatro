9bce6b8a6c6efeff034628794a36f8e96db823c1
unknown <imanol.mb@hotmail.com>
2025-09-30 18:37:04 -0300

    DB

--- tp2/src/models/task/interface/taskCrude.interface.ts
+++ tp2/src/models/task/interface/taskCrude.interface.ts
@@ -5,10 +5,10 @@ export interface TaskCrude<ID>{
     getTask(id: ID): Task<ID>;
     addTask(pizza: Task<ID>): void;
     deleteTask(id: ID): void;
-    editTask(id: ID, pizza: string): void;
-    editTask(id: ID, status: string): void;
-    editTask(id: ID, tamanio: string): void;
-    editTask(id: ID, toppings: string): void;
-    editTask(id: ID, precio: number): void;
+    editTask(id: ID, pizza: string): Task<ID>;
+    editTask(id: ID, status: string): Task<ID>;
+    editTask(id: ID, tamanio: string): Task<ID>;
+    editTask(id: ID, toppings: string): Task<ID>;
+    editTask(id: ID, precio: number): Task<ID>;
     editTask(id: ID, pizza: string, status: string, tamanio: string, toppings: string, precio: number): Task<ID>;
 }
--- tp2/src/models/task/mockTask.ts
+++ tp2/src/models/task/mockTask.ts
@@ -19,9 +19,9 @@ export class MockTask implements TaskCrude<number>{
     editTask(id: number, tamanio: string): Task<number>;
     editTask(id: number, toppings: string): Task<number>;
     editTask(id: number, precio: number): Task<number>;
-    editTask(id:number, precio:number, pizza:string, status:string, tamanio:string, toppings:string): Task<number>;
+    editTask(id:number, pizza:string, status:string, tamanio:string, toppings:string, precio:string): Task<number>;
     editTask(
-        id: unknown, pizza: unknown, status:unknown, tamanio:unknown, toppings:unknown, precio: unknown)
+        id: unknown, pizza: unknown, status?:unknown, tamanio?:unknown, toppings?:unknown, precio?: unknown)
         : import("./task").Task<number> {
         throw new Error("Method not implemented.");
     }
