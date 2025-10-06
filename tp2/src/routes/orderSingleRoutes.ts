0bdae2292041fb84d57d576bcbb5e79b9e5d4885
Giuseppe Giovanelli <giusegio2004@gmail.com>
2025-10-06 19:49:48 -0300

    Update orderSingleRoutes.ts

--- tp2/src/routes/orderSingleRoutes.ts
+++ tp2/src/routes/orderSingleRoutes.ts
@@ -1,8 +1,8 @@
-// import { Router } from 'express';
-// import { OrderController } from '../controllers/OrderController.js';
+import { Router } from 'express';
+import { OrderController } from '../controllers/OrderController.js';
 
-// export const orderSingleRoutes = Router();
-// const orderController = new OrderController();
+export const orderSingleRoutes = Router();
+const orderController = new OrderController();
 
-// // GET /order/:id - Obtener orden por ID (ruta singular)
-// orderSingleRoutes.get('/:id', orderController.getOrderById.bind(orderController));
+// GET /order/:id - Obtener orden por ID (ruta singular)
+orderSingleRoutes.get('/:id', orderController.getOrderById.bind(orderController));
