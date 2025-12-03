8373b09c71c81e79ec450a8a50e51d506339cb7e
Giuseppe Giovanelli <giusegio2004@gmail.com>
2025-12-03 16:40:32 -0300

    Update productController.js
    Refactored SQL query to use parameterized queries for security.

--- tp4/WebApp-Seguridad-Prog4/backend/src/controllers/productController.js
+++ tp4/WebApp-Seguridad-Prog4/backend/src/controllers/productController.js
@@ -1,21 +1,23 @@
 const { db } = require('../config/database');
 
-// VULNERABLE: SQL Injection
 const getProducts = (req, res) => {
   const { category, search } = req.query;
   
-  // VULNERABLE: Concatenación directa de strings en SQL
+  // SEGURO: Usando consultas parametrizadas
   let query = 'SELECT * FROM products WHERE 1=1';
+  let params = [];
   
   if (category) {
-    query += ` AND category = '${category}'`;
+    query += ' AND category = ?';
+    params.push(category);
   }
   
   if (search) {
-    query += ` AND name LIKE '%${search}%'`;
+    query += ' AND name LIKE ?';
+    params.push(`%${search}%`);
   }
   
-  db.query(query, (err, results) => {
+  db.query(query, params, (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
